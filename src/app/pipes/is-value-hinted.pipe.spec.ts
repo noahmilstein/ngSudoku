e6b5318ec0a3f7591d3a15fcb6f2e5c102f6e575
Noah Milstein <noahamilstein@gmail.com>
2021-08-03 12:16:52 -0400

    test is value hinted pipe

--- src/app/pipes/is-value-hinted.pipe.spec.ts
+++ src/app/pipes/is-value-hinted.pipe.spec.ts
@@ -1,8 +1,20 @@
+import { Cell } from '../models/cell.model'
 import { IsValueHintedPipe } from './is-value-hinted.pipe'
 
 describe('IsValueHintedPipe', () => {
+  const pipe = new IsValueHintedPipe()
+  const hintedCoordinates = [new Cell({ x: 0, y: 0 })]
+
   it('create an instance', () => {
-    const pipe = new IsValueHintedPipe()
     expect(pipe).toBeTruthy()
+  })
+
+  it('pipe transform should check if value in hinted', () => {
+    expect(pipe.transform(hintedCoordinates, 0, 0)).toBeTrue
+    expect(pipe.transform(hintedCoordinates, 1, 1)).toBeFalse
+  })
+
+  it('pipe transform should handle for null hints array', () => {
+    expect(pipe.transform(null, 1, 1)).toBeFalse
   })
 })
