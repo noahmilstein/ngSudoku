a5463669b8a7e16d75eb8e626b0f452d275df24e
Noah Milstein <noahamilstein@gmail.com>
2021-08-03 11:55:46 -0400

    test is cell active pipe

--- src/app/pipes/is-cell-active.pipe.spec.ts
+++ src/app/pipes/is-cell-active.pipe.spec.ts
@@ -1,8 +1,22 @@
+import { Cell } from '../models/cell.model'
 import { IsCellActivePipe } from './is-cell-active.pipe'
 
 describe('IsCellActivePipe', () => {
+  const pipe = new IsCellActivePipe()
+
   it('create an instance', () => {
-    const pipe = new IsCellActivePipe()
     expect(pipe).toBeTruthy()
+  })
+
+  it('pipe transform should check if cell is active', () => {
+    const activeCellCoordinates = new Cell({ x: 0, y: 0 })
+    expect(pipe.transform(activeCellCoordinates, 0, 0)).toBeTrue
+
+    const inactiveCellCoordinates = new Cell({ x: 1, y: 0 })
+    expect(pipe.transform(inactiveCellCoordinates, 0, 0)).toBeFalse
+  })
+
+  it('pipe transform should handle for null coordinates', () => {
+    expect(pipe.transform(null, 0, 0)).toBeFalse
   })
 })
