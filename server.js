91ddb269b97024926c78c8473f02682f564e262e
Noah Milstein <noahamilstein@gmail.com>
2021-08-07 15:48:20 -0400

    heroku deployment fix

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var path = require('path')
 var app = express()
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'))
+app.use(express.static(__dirname + '/dist/ngSudoku'))
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/ngSudoku/index.html'))
 })
 
 // Start the app by listening on the default Heroku port
